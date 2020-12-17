7720da6e176ca1deef6ba1219717240362e949ec
Austin Cudworth <austincudworth@gmail.com>
2020-12-16 21:16:51 -0800

    Add state for tracking multiple hotdogs and which have been found

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
-//import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header';
 //import Footer from './Footer/Footer';
 import Image from './Image/Image';
+import cloneDeep from 'lodash/cloneDeep';
 
-import sampleImage from './assets/food-2379472_1920.jpg';
+import localImage from './assets/food-2379472_1920.jpg';
 
-function onImageClick(e) {
-  //get click coordinates relative to page
-  const rect = e.target.getBoundingClientRect();
+import { useState } from 'react';
 
-  //Calc click location within element, normalized for image height
-  const unit = rect.height;
-  const pLeft = (e.pageX - rect.left - window.scrollX) / unit;
-  const pTop = (e.pageY - rect.top - window.scrollY) / unit;
-  return [pTop, pLeft];
-}
+//Data format y-coord, x-coord, radius normalized for image height
+const myImage = {
+  url: localImage,
+  dogs: [
+    { y: 0.32355555555555554, x: 1.0844444444444445, r: 0.12 },
+    { y: 0.128, x: 1.408, r: 0.12 },
+    { y: 0.48355555555555557, x: 1.5715555555555556, r: 0.12 },
+    { y: 0.8924444444444445, x: 1.4151111111111112, r: 0.12 },
+  ],
+};
 
 function App() {
+  const [state, setState] = useState({
+    dogsFound: [].fill(false, 0, myImage.dogs.length),
+    dogs: myImage.dogs,
+  });
+
+  function isHotDog(coords, hotdogs) {
+    const [y1, x1] = coords;
+    const arr = hotdogs.map((hotdog) => {
+      const { y, x, r } = hotdog;
+      const r1 = ((y1 - y) ** 2 + (x1 - x) ** 2) ** (1 / 2);
+      return r1 < r ? true : false;
+    });
+    return arr;
+  }
+
+  function onImageClick(e) {
+    //get click coordinates relative to page
+    const rect = e.target.getBoundingClientRect();
+
+    //Calc click location within element, normalized for image height
+    //scale = rect.height;
+    const normY = (e.pageY - rect.top - window.scrollY) / rect.height;
+    const normX = (e.pageX - rect.left - window.scrollX) / rect.height;
+    const coords = [normY, normX];
+    //console.log(coords);
+
+    setState((prev) => {
+      const next = cloneDeep(prev);
+      next.scale = rect.height;
+      const hits = isHotDog(coords, myImage.dogs);
+      hits.forEach((hit, i) => {
+        if (hit && !next.dogsFound[i]) {
+          next.dogsFound[i] = true;
+        }
+      });
+      return next;
+    });
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <Image url={sampleImage} handleClick={onImageClick}></Image>
+      <Image
+        url={myImage.url}
+        handleClick={onImageClick}
+        scale={state.scale}
+        dogs={state.dogs}
+        dogsFound={state.dogsFound}
+      ></Image>
     </div>
   );
 }
 
 export default App;
-
-/*
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-*/
--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,13 +1,41 @@
+import { useEffect } from 'react';
 import './Image.css';
 
 function Image(props) {
+  useEffect(() => {
+    drawCircles();
+  });
+
+  function drawCircles() {
+    const c = document.querySelector('canvas');
+    const img = document.querySelector('img');
+    c.width = img.width;
+    c.height = img.height;
+    const ctx = c.getContext('2d');
+    props.dogsFound.forEach((dogFound, i) => {
+      if (dogFound) {
+        const { y, x, r } = props.dogs[i];
+        ctx.beginPath();
+        ctx.arc(
+          x * props.scale,
+          y * props.scale,
+          r * props.scale,
+          0,
+          2 * Math.PI
+        );
+        ctx.lineWidth = 6;
+        ctx.strokeStyle = 'chartreuse';
+        ctx.stroke();
+      }
+    });
+  }
+
   return (
     <div className="Image">
-      <img
-        src={props.url}
-        alt="[Various foods]"
-        onClick={(e) => props.handleClick(e)}
-      ></img>
+      <div>
+        <img src={props.url} alt="[Various foods]"></img>
+        <canvas onClick={(e) => props.handleClick(e)}></canvas>
+      </div>
     </div>
   );
 }
