c9ec427aef7eddd53386759e95addf01300fef70
Austin Cudworth <austincudworth@gmail.com>
2021-01-24 16:34:39 -0800

    Update firebase module imports for deployment

--- src/firebaseModule.js
+++ src/firebaseModule.js
@@ -1,4 +1,8 @@
-import firebase from 'firebase';
+//import firebase from 'firebase';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = require('./firebaseKeys.json');
 
